5446f775f96ade42d6e4fe36fd77d0399992fd0b
xifengxiaoyun <3775538464@qq.com>
2025-11-01 14:27:11 +0800

    Great sword finished
    Signed-off-by: xifengxiaoyun <3775538464@qq.com>

--- src/main/java/com/xifeng/tinkersidea/TinkersIdea.java
+++ src/main/java/com/xifeng/tinkersidea/TinkersIdea.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
+//
 @Mod(
         modid = Tags.MOD_ID,
         name = Tags.MOD_NAME,
